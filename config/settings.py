20fed54c3fad71d25b7dc8af757b5fb3f38848b8
Layth Zahid <laith43d@gmail.com>
2021-09-02 00:58:12 +0300

    Skeleton - v1.4

--- config/settings.py
+++ config/settings.py
@@ -2,6 +2,8 @@ import os
 from pathlib import Path
 
 import pretty_errors
+from decouple import config
+from dj_database_url import parse as db_url
 
 pretty_errors.configure(
     separator_character='*',
@@ -23,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-076yvonm0xalc)5e%iu!!i_vabq!4tn^l__2t7(6i)8^xey_o1'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
@@ -87,10 +89,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': config(
+        'DATABASE_URL',
+        cast=db_url
+    )
 }
 
 # Password validation
